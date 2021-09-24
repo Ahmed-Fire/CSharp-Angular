baf472fdcd1336f70a45dee5be498a4d12fa20bf
“Ahmed-Fire” <ahmad.emad4@gmail.com>
2021-09-25 01:00:18 +0300

    Create Program.cs

---
+++ donguler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
