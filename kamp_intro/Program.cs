62b08343caadf703c8729bd06bf65e0c208b956c
“Ahmed-Fire” <ahmad.emad4@gmail.com>
2021-09-25 01:06:43 +0300

    Create Program.cs

---
+++ kamp_intro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace kampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safe
+            string categories = "category";
+            int orenciSayisi = 10000;
+            double faizOrani = 1.45;
+            bool isSigned = true;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalis Butonu");
+            }
+            else if (dolarDun < dolarBugun)
+            {
+                Console.WriteLine("Artis Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Degismedi Butonu");
+            }
+
+            if (isSigned == true)
+            {
+                Console.WriteLine("Setting Button");
+            }
+            else
+            {
+                Console.WriteLine("Sign Button");
+            }
+
+            Console.WriteLine(categories);
+        }
+    }
+}
